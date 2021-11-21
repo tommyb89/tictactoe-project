87ad9d84a063330cb6c5ddb88d92c40ddc35d0db
Tomasz <mandalorian99@gmail.com>
2021-11-21 21:25:35 +0000

    a loong function to check who wins

--- main.js
+++ main.js
@@ -37,4 +37,82 @@ const checkGameStatus = () => {
   const bottomLeft = tiles[6].classList[1];
   const bottomMiddle = tiles[7].classList[1];
   const bottomRight = tiles[8].classList[1];
+
+  console.log(bottomRight);
+
+  // check the winner
+
+  if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+    tiles[0].classList.add("won");
+    tiles[1].classList.add("won");
+    tiles[2].classList.add("won");
+  } else if (
+    middleLeft &&
+    middleLeft === middleMiddle &&
+    middleLeft === middleRight
+  ) {
+    handleWin(middleLeft);
+    tiles[3].classList.add("won");
+    tiles[4].classList.add("won");
+    tiles[5].classList.add("won");
+  } else if (
+    bottomLeft &&
+    bottomLeft === bottomMiddle &&
+    bottomLeft === bottomRight
+  ) {
+    handleWin(bottomLeft);
+    tiles[6].classList.add("won");
+    tiles[7].classList.add("won");
+    tiles[8].classList.add("won");
+  } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+    handleWin(topLeft);
+    tiles[0].classList.add("won");
+    tiles[3].classList.add("won");
+    tiles[6].classList.add("won");
+  } else if (
+    topMiddle &&
+    topMiddle === middleMiddle &&
+    topMiddle === bottomMiddle
+  ) {
+    handleWin(topMiddle);
+    tiles[1].classList.add("won");
+    tiles[4].classList.add("won");
+    tiles[7].classList.add("won");
+  } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+    handleWin(topRight);
+    tiles[2].classList.add("won");
+    tiles[5].classList.add("won");
+    tiles[8].classList.add("won");
+  } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+    handleWin(topLeft);
+    tiles[0].classList.add("won");
+    tiles[4].classList.add("won");
+    tiles[8].classList.add("won");
+  } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+    handleWin(topRight);
+    tiles[2].classList.add("won");
+    tiles[4].classList.add("won");
+    tiles[6].classList.add("won");
+  } else if (
+    topLeft &&
+    topMiddle &&
+    topRight &&
+    middleLeft &&
+    middleMiddle &&
+    middleRight &&
+    bottomLeft &&
+    bottomMiddle &&
+    bottomRight
+  ) {
+    gameIsLive = false;
+    statusBox.innerHTML = "Game is tied!";
+  } else {
+    xIsNext = !xIsNext;
+    if (xIsNext) {
+      statusBox.innerHTML = `${x} is next`;
+    } else {
+      statusBox.innerHTML = `<span>${o} is next</span>`;
+    }
+  }
 };
